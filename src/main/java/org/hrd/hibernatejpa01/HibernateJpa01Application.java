988da15b6fefa519ad221729c5897db9c8474070
PathNimol <pathnimol0425@gmail.com>
2025-08-06 23:38:01 +0700

    first commit

---
+++ src/main/java/org/hrd/hibernatejpa01/HibernateJpa01Application.java
@@ -0,0 +1,13 @@
+package org.hrd.hibernatejpa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpa01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateJpa01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/hrd/hibernatejpa01/HibernateJpa01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hrd.hibernatejpa01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpa01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
